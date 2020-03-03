6643095b1816ab3387011ab3e4c5f4b16208a5c1
Noah Franco <noahfranco@jonadabs-mini.lan>
2020-03-03 13:42:28 -0500

    finished register page

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import {BrowserRouter as Router, Router, Route} from "react-router-dom";
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import Home from './components/home.js';
 import Login from "./components/login.js";
 import Register from "./components/register.js"; 
@@ -9,9 +9,9 @@ function App() {
   return (
     <div className="App">
       <Router>
-        <Route exact path="/" component={Home}  />
-        <Route path="/login" component={Login} />
+        <Route exact path="/" component={Login}  />
         <Route path="/register" component={Register} />
+        <Route path="/home" component={Home} />
       </Router>
     </div>
   );
--- client/src/components/register.js
+++ client/src/components/register.js
@@ -1,10 +1,63 @@
-import React from "react"; 
+import React, {useState} from "react"; 
+import axios from "axios"
+// import {BrowserRouter as Link} from "react-router-dom";
 
-function Register() {
+function Register(props) {
     
+    const [register, setRegister] = useState({
+        username: "",
+        password: ""
+    })
+
+    const handleSubmit = event => {
+        event.preventDefault()
+        axios
+        .post(" ")
+        .then(res => {
+            // pass token 
+            // api api data to state
+            props.history.push("/home")
+        })
+        .catch(error => {
+            console.log("error from register components",error)
+        },[])
+    }
+
+    const handleChange = event => {
+        setRegister({
+            ...register,
+            [event.target.name]: event.target.value
+        })
+    }
+
     return(
         <>
         <h2> Register Component </h2>
+        <div> 
+            <form onSubmit={handleSubmit}>
+                <label> Username </label> 
+                <input
+                type="text"
+                name="username"
+                placeholder="enter username"
+                value={props.username}
+                onChange={handleChange}
+                required
+                />
+                <label> Password </label>
+                <input
+                type="password"
+                name="password"
+                placeholder="enter password"
+                value={props.password}
+                onChange={handleChange}
+                required
+                />
+                <div> 
+                <button> Register </button> 
+                </div>
+            </form>
+        </div>
         </>
     )
 }
