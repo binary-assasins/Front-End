2d9860886efe8bbfc68d5f428c83c2bf213f0bff
Noah Franco <noahfranco@jonadabs-mini.lan>
2020-03-04 23:41:14 -0500

    working on Map.js

--- client/src/components/MapThrough.js
+++ client/src/components/MapThrough.js
@@ -1,13 +1,15 @@
 import React from "react"; 
 
 function MapThrough(props) {
-    
+    console.log("This is props from MapThrough", props)
     return(
         <>
+        <p> Something!! </p>
+        {/* <div> {props.id} </div>
         <div> {props.title} </div> 
         <div> {props.x} </div> 
         <div> {props.y} </div> 
-        <div> {props.description} </div> 
+        <div> {props.description} </div>  */}
         </>
     )
 }
