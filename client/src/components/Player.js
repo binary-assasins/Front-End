813b8c270411e19826efbd728a43b305cd585142
MadisonViaLux <madison.s.mckown@gmail.com>
2020-03-05 20:39:46 -0700

    styling update

--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -1,5 +1,6 @@
 import React, {useEffect, useState} from "react"
 import axiosWithAuth from "./auth"
+import '../css/player.css'
 
 function Player() {
 
@@ -10,6 +11,7 @@ useEffect(() => {
     axiosWithAuth()
     .get("https://binary-assassins.herokuapp.com/api/adv/init", player)
     .then(res => {
+        // console.log(res)
         setPlayer(res.data)
     })
     .catch(error => {
@@ -31,6 +33,7 @@ const moving = (i) => {
        }) 
     // })
 }
+
 // console.log(player)
 
 // call the init end point to initialize where the player is sponds
@@ -40,19 +43,31 @@ const moving = (i) => {
 
     //Remember North is south and South is north...
     return(
-        <>
-        <div>
-            <button onClick={ () => {moving('n')}}> S </button> 
-            <button onClick={ () => {moving('s')}}> N </button>
-            <button onClick={ () => {moving('e')}}> E </button>
-            <button onClick={ () => {moving('w')}}> W </button>
-           
-        </div>
-            <div>
-                <p>Room Number: {player.title}</p>
-                <p>Room Description: {player.biome}</p>
+        <div className="ControlHub">
+
+            <div className="NavButtons">
+                <button onClick={ () => {moving('n')}}> North (n) </button> 
+                <button onClick={ () => {moving('s')}}> South (s) </button>
+                <button onClick={ () => {moving('e')}}> East (e) </button>
+                <button onClick={ () => {moving('w')}}> West (w) </button>
+                <p>You can move: {player.room_directions}</p>
+            
             </div>
-        </>
+
+            <div className="PlayerInfo">
+                <div>
+                    <p>{player.name}</p>
+                    <p>Current inventory: {player.inventory}</p>
+                </div>
+                <div>
+                    <p>You are in Room Number: {player.title}</p>
+                    <p>Room {player.title}'s biome is the *{player.biome}* biome!</p>
+                    <p>Players in room {player.title}: {player.players}</p>
+                </div>
+                <p>{player.error_msg}</p> 
+            </div>
+
+        </div>
     )
 }
 export default Player
